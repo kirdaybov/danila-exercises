059b8689c3072c38330c716a7653e72b1bb1baec
DanDaybov <danilkadaibov@gmail.com>
2023-01-30 21:46:35 +0400

    victori ch3

--- ch3/ex14.py
+++ ch3/ex14.py
@@ -1,11 +1,10 @@
-mass = int(input('Введите масу тела -\n'))
-height = int(input('Введите рост -\n'))
+mass = float(input('Введите масу тела в кг -\n'))
+height = float(input('Введите рост в метрах-\n'))
 body_mass_index = mass / (height**2) 
 print ('Ваш ИМТ - ', body_mass_index)
 if 18.5 <= body_mass_index <= 25:
-    print('Ваша масса  оптимальна')
+    print('Ваша масса оптимальна')
 elif body_mass_index < 18.5:
     print('Весите меньше нормы')
-else:
-    body_mass_index > 25
+else: 
     print ('Весите больше нормы')
--- ch3/ex15.py
+++ ch3/ex15.py
@@ -1,16 +1,21 @@
 second = int(input('Введите кол-во секунд - \n '))
-if  60 >= second < 3600:
+if  60 <= second < 3600:
     minute = second // 60
     second = second % 60
     print('Число минут равно - ', minute, '\nЧисло секунд равно - ', second)
-elif 3600 >= second < 86400:
-    minute = second % 3600
-    second = second % 60
+elif 3600 <= second <= 86400:
     hour = second // 3600
+    second = second - (hour * 3600)
+    minute = second // 60
+    second = second - (minute * 60)
     print('Число часов равно - ', hour,'Число минут равно - ', minute, '\nЧисло секунд равно - ', second)
-elif second >= 86400:
-    day = second % (24 * 3600)
+elif second > 86400:
+    day = second // (24 * 3600)
+    second = second - (day * 24 * 3600)
     hour = second // 3600
-    minute = second % 3600
-    second = second % 60
+    second = second - (hour * 3600)
+    minute = second // 60
+    second = second - (minute * 60)
     print('Число дней равно - ', day,'Число часов равно - ', hour,'Число минут равно - ', minute, '\nЧисло секунд равно - ', second)
+else:
+    print('Число секунд - ', second)
